e3af49d38e9765ea663e4a6e0ec4c7d5ec481707
fulvio.zanetti <fulvio.zanetti@gmail.com>
2019-07-05 10:54:13 +0200

    component library creation

---
+++ src/lib-dev.vue
@@ -0,0 +1,23 @@
+<script>
+import { VueCommercioUiSample } from './entry';
+export default {
+    name:  'VueCommercioUiLibDev', // vue library dev component
+    components: {
+        VueCommercioUiSample,
+    },
+}
+</script>
+
+<template>
+    <div class="vue-commercio-ui-lib-dev">
+        <vue-commercio-ui-sample></vue-commercio-ui-sample>
+    </div>
+</template>
+
+<style scoped>
+    .vue-commercio-ui-lib-dev {
+        display: block;
+        width: 400px;
+        margin: 25px auto;
+    }
+</style>
