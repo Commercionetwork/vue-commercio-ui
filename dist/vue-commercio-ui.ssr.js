41699976cec996ddeb5f6db300b26b9bd64ff9f4
fulvio.zanetti <fulvio.zanetti@gmail.com>
2019-07-15 11:28:08 +0200

    Added css

--- dist/vue-commercio-ui.ssr.js
+++ dist/vue-commercio-ui.ssr.js
@@ -262,17 +262,17 @@ var __vue_render__$1 = function () {var _vm=this;var _h=_vm.$createElement;var _
 var __vue_staticRenderFns__$1 = [];
 
   /* style */
-  var __vue_inject_styles__$1 = undefined;
+  var __vue_inject_styles__$1 = function (inject) {
+    if (!inject) { return }
+    inject("data-v-04c75724_0", { source: "@import url(https://fonts.googleapis.com/css?family=Open+Sans|Roboto+Mono);#app,body,html{height:100%;background-color:#ededed}a,h1{color:#38ba8c}a:hover{color:#38ba8c;text-decoration:none}h2,h3,h4,h5,h6{color:#202020}div,p,span{color:#202020}.com-pointer{cursor:pointer}.com-nowrap{white-space:nowrap}.com-scroll-y{overflow-y:scroll}.com-required-field label::after{content:\"*\";color:#e30;margin-left:.25rem}.com-required-field-error{margin-bottom:3px;position:absolute;bottom:-3px;left:0}.com-white-background{background-color:#fff}.com-gray-darkest{color:#202020}.com-gray-darker{color:#3c3c3c}.com-gray-dark{color:#4f4f4f}.com-gray{color:#6f6f6f}.com-gray-light{color:#7e7e7e}.com-gray-lighter{color:#ededed}.com-gray-lightest{color:#fafafa}.com-font-s30-w900{font-size:30px;font-weight:900}.com-font-s20-w800{font-size:20px;font-weight:800}.com-font-s16-w700{font-size:16px;font-weight:700}.com-font-s14-w700{font-size:14px;font-weight:700}.com-font-s14-w400{font-size:14px;font-weight:400}.com-font-s14-w300{font-size:14px;font-weight:300}.com-font-s13-w700{font-size:13px;font-weight:700}.com-font-s13-w400{font-size:13px;font-weight:400}.com-font-s12-w700{font-size:12px;font-weight:700}.com-font-s12-w400{font-size:12px;font-weight:400}.com-font-s12-w300{font-size:12px;font-weight:300}.com-font-s11-w700{font-size:11px;font-weight:700}.com-font-s11-w400{font-size:11px;font-weight:400}.com-font-s10-w700{font-size:10px;font-weight:700}.com-border-0010{border-width:0 0 1px 0}.com-border-5-lighter{border:5px solid #ededed}.com-border-5-dashed{border:5px dashed #ededed}.com-border-2-dashed{border:2px dashed #7e7e7e}.com-svg svg{width:100%;height:100%}.com-image-h35-w35{height:35px;width:35px}.com-image-h50-w50{height:50px;width:50px}.com-image-h100-w100{height:100px;width:100px}.com-container-w580{max-width:580px}.com-container-w740{max-width:740px}.com-dashboard-row{display:flex;flex-wrap:wrap}.com-dashboard-row>div[class*=col-]{display:flex}.com-dashboard-card,.com-dashboard-card-header{border-radius:10px}.com-dashboard-card{height:215px;width:160px}.com-modal-big{min-width:90vw}.com-modal-big .modal-content{min-height:90vh}.com-modal-small{min-width:45vw}.com-modal-small .modal-content{min-height:45vh}.com-modal-big .modal-footer,.com-modal-big .modal-header,.com-modal-small .modal-footer,.com-modal-small .modal-header{border:none}.slidecontainer{width:100%}.slider{-webkit-appearance:none;width:100%;height:15px;border-radius:5px;background:#ededed;outline:0;opacity:.7;-webkit-transition:.2s;transition:opacity .2s}.slider:hover{opacity:1}.slider::-webkit-slider-thumb{-webkit-appearance:none;appearance:none;width:25px;height:25px;border-radius:50%;background:#38ba8c;cursor:pointer}.slider::-moz-range-thumb{width:25px;height:25px;border-radius:50%;background:#38ba8c;cursor:pointer}", map: undefined, media: undefined });
+
+  };
   /* scoped */
   var __vue_scope_id__$1 = undefined;
   /* module identifier */
-  var __vue_module_identifier__$1 = "data-v-420dd590";
+  var __vue_module_identifier__$1 = "data-v-04c75724";
   /* functional template */
   var __vue_is_functional_template__$1 = false;
-  /* style inject */
-  
-  /* style inject SSR */
-  
 
   
   var ListView = normalizeComponent_1(
@@ -283,7 +283,7 @@ var __vue_staticRenderFns__$1 = [];
     __vue_is_functional_template__$1,
     __vue_module_identifier__$1,
     undefined,
-    undefined
+    server
   );/* eslint-disable import/prefer-default-export */var components = /*#__PURE__*/Object.freeze({VueCommercioUiSample: vueCommercioUiSample,ListView: ListView});// Import vue components
 
 // install function executed by Vue.use()
--- src/lib-components/ListView.vue
+++ src/lib-components/ListView.vue
@@ -85,3 +85,9 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+@import "../assets/scss/custom-bootstrap.scss";
+// @import "node_modules/bootstrap/scss/bootstrap.scss";
+@import "../assets/scss/style.scss";
+</style>
