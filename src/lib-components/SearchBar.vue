a4b1654b3d3c8260d4ff2ee41e73872286b1f581
fulvio <fulvio.zanetti@gmail.com>
2019-07-19 09:33:42 +0200

    Added search bar

---
+++ src/lib-components/SearchBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row d-flex align-items-center">
+    <div class="col-2 col-sm-1 d-flex justify-content-center">
+      <span class="p-1">
+        <Icon
+          name="filter"
+          scale="2"
+          class="text-black-50"
+        />
+      </span>
+    </div>
+    <div class="col-8 col-sm-10 d-flex justify-content-center">
+      <input
+        class="form-control bg-light rounded-pill com-font-s14-w400"
+        :placeholder="searchMessage"
+      />
+    </div>
+    <div class="col-2 col-sm-1 d-flex justify-content-center">
+      <span class="p-1">
+        <slot name="icon" />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  description: "Display the search bar",
+  props: {
+    searchMessage: {
+      type: String,
+      default: "Search...",
+      note: "The search placeholder"
+    }
+  }
+};
+</script>
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -2,4 +2,5 @@
 export { default as VueCommercioUiSample } from './vue-commercio-ui-sample.vue';
 export { default as ListView } from './ListView.vue';
 export { default as NavBar } from './NavBar.vue';
+export { default as SearchBar } from './SearchBar.vue';
 
