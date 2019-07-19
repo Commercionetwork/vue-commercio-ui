6e094fb1720b6f81658dc822b74215679fc7b209
fulvio <fulvio.zanetti@gmail.com>
2019-07-19 09:48:32 +0200

    Fixed seacrh bar

--- src/lib-components/SearchBar.vue
+++ src/lib-components/SearchBar.vue
@@ -2,11 +2,7 @@
   <div class="row d-flex align-items-center">
     <div class="col-2 col-sm-1 d-flex justify-content-center">
       <span class="p-1">
-        <Icon
-          name="filter"
-          scale="2"
-          class="text-black-50"
-        />
+        <slot name="icon-left" />
       </span>
     </div>
     <div class="col-8 col-sm-10 d-flex justify-content-center">
@@ -17,7 +13,7 @@
     </div>
     <div class="col-2 col-sm-1 d-flex justify-content-center">
       <span class="p-1">
-        <slot name="icon" />
+        <slot name="icon-right" />
       </span>
     </div>
   </div>
