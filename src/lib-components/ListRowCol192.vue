109dde30bd7c3f2d2e2b96ede7bcb2d25825cc63
fulvio <fulvio.zanetti@gmail.com>
2019-07-19 10:16:19 +0200

    Added list row components

---
+++ src/lib-components/ListRowCol192.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row py-1 d-flex flex-row align-items-center">
+    <!-- Left content -->
+    <div class="col-12 col-md-1 py-1 py-md-0">
+      <slot name="left-content"/>
+    </div>
+    <!-- Main content -->
+    <div class="col-12 col-md-9 py-1 py-md-0">
+      <slot name="main-content"/>
+    </div>
+    <!-- Separator (showed on small viewport) -->
+    <div class="col-12 d-md-none">
+      <hr>
+    </div>
+    <!-- Right content -->
+    <div class="col-12 col-md-2 py-1 py-md-0">
+      <slot name="right-content"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListRowCol192",
+  description: "Display list row"
+}
+</script>
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,6 +1,25 @@
 /* eslint-disable import/prefer-default-export */
-export { default as VueCommercioUiSample } from './vue-commercio-ui-sample.vue';
-export { default as ListView } from './ListView.vue';
-export { default as NavBar } from './NavBar.vue';
-export { default as SearchBar } from './SearchBar.vue';
-
+export {
+  default as VueCommercioUiSample
+}
+from './vue-commercio-ui-sample.vue';
+export {
+  default as ListRowCol183
+}
+from './ListRowCol183.vue';
+export {
+  default as ListRowCol192
+}
+from './ListRowCol192.vue';
+export {
+  default as ListView
+}
+from './ListView.vue';
+export {
+  default as NavBar
+}
+from './NavBar.vue';
+export {
+  default as SearchBar
+}
+from './SearchBar.vue';
